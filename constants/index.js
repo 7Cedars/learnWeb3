d4a7792b436afe1f7947c82f3eff4e644adce1f7
7Cedars <Cedars7@proton.me>
2022-09-26 11:31:09 +0100

    fourth commit

--- constants/index.js
+++ constants/index.js
@@ -581,4 +581,4 @@ export const abi = [
       "type": "receive"
     }
   ]
-export const NFT_CONTRACT_ADDRESS = "0x2a794bD9FCCf802b0f2BDe185bb273D9C9015067"
+export const NFT_CONTRACT_ADDRESS = "0x144b96d96FcF114286A96975a0386bcD8941a205"
